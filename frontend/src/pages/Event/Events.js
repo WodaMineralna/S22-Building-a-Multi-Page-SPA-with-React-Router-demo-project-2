51108abc0b4ff249e41935b8d5b267debab7c7c3
WodaMineralna <raknieborak65@gmail.com>
2025-06-12 20:52:22 +0200

    Deferring Data Fetching (the events), so the UI and UX is better

--- frontend/src/pages/Event/Events.js
+++ frontend/src/pages/Event/Events.js
@@ -1,24 +1,38 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Await } from "react-router-dom";
 
 import EventsList from "../../components/EventsList";
+import { Suspense } from "react";
 
-function EventsPage() {
+export default function EventsPage() {
   const data = useLoaderData();
   const eventsData = data.events;
 
-  return <EventsList events={eventsData} />;
+  return (
+    <Suspense
+      fallback={<p style={{ textAlign: "center" }}>Loading Events data...</p>}
+    >
+      <Await resolve={eventsData}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
-export default EventsPage;
-
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://192.168.1.18:8080/events");
 
   if (!response.ok) {
     throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
       status: 500,
     });
-  } else {
-    return response;
   }
+
+  const resData = await response.json();
+  return resData.events;
+}
+
+export async function loader() {
+  return {
+    events: loadEvents(),
+  };
 }
